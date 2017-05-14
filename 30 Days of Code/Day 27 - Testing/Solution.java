eb2052ff5948c618844c0b9bff73ca85b2397f4e
Koushik Thai <thaikoushik@gmail.com>
2017-05-14 15:27:40 -0400

    Create Solution.java

---
+++ 30 Days of Code/Day 27 - Testing/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   System.out.println(5);
+
+        System.out.println("4 3");
+        System.out.println("-1 0 1 2");
+        
+        System.out.println("3 1");
+        System.out.println("-1 0 1");
+        
+        System.out.println("5 4");
+        System.out.println("-1 0 1 2 3");
+        
+        System.out.println("6 1");
+        System.out.println("-1 0 1 2 3 4");
+        
+        System.out.println("7 6");
+        System.out.println("-1 0 1 2 3 4 5");
+    }
+}
