b8380e700d1e039048549ec8f3cd4f1009245b3c
Koushik Thai <thaikoushik@gmail.com>
2017-05-14 15:27:11 -0400

    Create Solution.java

---
+++ 30 Days of Code/Day 26 - Nested Loginc/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        int d1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        
+        int d2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int y2 = sc.nextInt();
+      
+        if(y1>y2){
+            System.out.println(10000);
+        } else if(y1<y2){
+            System.out.println(0);
+        } else if(y2==y1){
+            if((m1<m2)){
+                System.out.println(0);
+            }else if(m1>m2) {
+                System.out.println(500* Math.abs(m2-m1));
+            }else if(m2 == m1){
+                if(d1<d2){
+                    System.out.println(0);
+                } else if(d1>d2){
+                    System.out.println(15*Math.abs(d2-d1));
+                } else if(d2==d1){
+                    System.out.println(0);
+                }
+            }
+        }
+    
+        
+    }
+}
