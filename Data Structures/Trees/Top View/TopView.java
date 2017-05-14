ef02fff1fbbe6c8df007bd07c784fe27a666d6a8
Koushik Thai <thaikoushik@gmail.com>
2017-05-14 15:03:57 -0400

    Create TopView.java

---
+++ Data Structures/Trees/Top View/TopView.java
@@ -0,0 +1,28 @@
+/*
+   class Node 
+       int data;
+       Node left;
+       Node right;
+*/
+void top_view(Node root)
+{
+    if(root!=null){
+    printLeft(root.left);
+    System.out.print(root.data+" ");
+    printRight(root.right);
+    }
+}
+    void printLeft(Node n){
+        if(n!=null){
+            printLeft(n.left);
+            System.out.print(n.data+" ");
+        }
+    }
+    
+    void printRight(Node n){
+        if(n!=null){
+            
+            System.out.print(n.data+" ");
+            printRight(n.right);
+        }
+    }
