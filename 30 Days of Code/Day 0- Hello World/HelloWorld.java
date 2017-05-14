c2c2898b0e97fa0536d219653c3f4984c9e5d44d
Koushik Thai <thaikoushik@gmail.com>
2017-05-14 15:09:16 -0400

    Create HelloWorld.java

---
+++ 30 Days of Code/Day 0- Hello World/HelloWorld.java
@@ -0,0 +1,12 @@
+public class Solution {
+	public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
+		
+		String inputString = scan.nextLine(); 
+    scan.close(); 
+      
+		System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
