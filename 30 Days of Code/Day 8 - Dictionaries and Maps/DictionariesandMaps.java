3918a40c3bd6484565638f2191f3a5e9de3301dc
Koushik Thai <thaikoushik@gmail.com>
2017-05-14 15:14:23 -0400

    Create DictionariesandMaps.java

---
+++ 30 Days of Code/Day 8 - Dictionaries and Maps/DictionariesandMaps.java
@@ -0,0 +1,26 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        HashMap<String,Integer> phoneBook = new HashMap<String,Integer>();
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name,phone);
+            // Write code here
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(phoneBook.containsKey(s)){
+                System.out.println(s+"="+phoneBook.get(s));
+            }
+            else System.out.println("Not found");
+            // Write code here
+        }
+        in.close();
+    }
+}
